729f62f113a2969c59e6ec6bf088df70044df534
Theo <theo.acooper@gmail.com>
2019-07-23 11:39:19 -0400

    cut default camera resolution in half for realtime streaming

--- codebase/CORE/parameters.py
+++ codebase/CORE/parameters.py
@@ -3,7 +3,7 @@ VERBOSE = False
 BYPASS_GAMEPAD = False
 BYPASS_STREAMING = False
 # Change the camera resolution, before the processes start
-CAM_WIDTH = 640
-CAM_HEIGHT = 480
+CAM_WIDTH = 320
+CAM_HEIGHT = 240
 
 
