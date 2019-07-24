84fc5aa5d96c92a44b8289ed9d546c188b68fade
Theo <theo.acooper@gmail.com>
2019-07-24 06:34:23 -0400

    OpenCv test

--- codebase/pipeline.py
+++ codebase/pipeline.py
@@ -6,8 +6,11 @@ import numpy as np
 import cv2
 from multiprocessing import Queue
 def pipeline(image, motorq):
-    imageOut = image
+
+    imageOut = cv2.GaussianBlur(image,[3,3],0)
+
     pass # replace with your code
+
     return imageOut # to help debug your code, we recommend you mark up the given image and return it here to see it over the stream
 
 # you will likely needs to separate your code into various functions called within pipeline
