98af51fabd2eaeec1acf454e9f1227ffb3aa9c5c
Theo <theo.acooper@gmail.com>
2019-07-25 09:43:15 -0400

    made code more user friendly

--- codebase/videoclient.py
+++ codebase/videoclient.py
@@ -1,10 +1,14 @@
 from CORE.rpistreaml.streamclient import Client
+import socket
 import cv2
 
-SERVER_IP = "localhost"
+SERVER_IP = "YOURIPHERE"
 SERVER_PORT = 5000
 #NOTE: if you change the resolution in the server, you have to change it in the client
-client = Client(serverIp=SERVER_IP, port = SERVER_PORT, promoteErrors = True, writeFile = True) # Connects to the server
+try:
+    client = Client(serverIp=SERVER_IP, port = SERVER_PORT, promoteErrors = True, writeFile = True) # Connects to the server
+except socket.error:
+    print "Error connecting to duckybot, make sure that your raspberry pi is running your program and you have editted this file to contain the IP address of your duckybot"
 client.initializeStream()
 try:
     try:
