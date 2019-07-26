18859a48fb0fe3ef58b61710df02e83b0cfee97a
Theo <theo.acooper@gmail.com>
2019-07-26 00:25:27 -0400

    extracted parameters file - needs testing

--- codebase/main.py
+++ codebase/main.py
@@ -1,4 +1,12 @@
+# in order to write parameters for the CORE module without being inside the CORE module
+# we copy our local parameters file and clone it into a parameters.py file within the CORE module, before we init the module
+coreParamFile = open("./CORE/parameters.py", "w")
+lParamFile = open("parameters.py", "r")
+lines = lParamFile.readlines()
+coreParamFile.writelines(lines)
+coreParamFile.close()
+
 import pipeline
-import CORE.main
+import CORE.main # this will now reference the clone of our local param file within the CORE module
 
 CORE.main.start(pipeline.pipeline) # entry point
--- codebase/parameters.py
+++ codebase/parameters.py
@@ -1,9 +1,8 @@
 # this file contains useful constants for the rest of the codebase
 VERBOSE = False
-BYPASS_GAMEPAD = False
-BYPASS_STREAMING = False
+DISABLE_GAMEPAD = False
+DISABLE_STREAMING = False
+DISABLE_MOTORS = False
 # Change the camera resolution, before the processes start
 CAM_WIDTH = 320
 CAM_HEIGHT = 240
-
-
